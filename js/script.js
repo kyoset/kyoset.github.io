7419a604c80d85edd4394c4c553fd5f73ea1dca2
kyoset <46853509+kyoset@users.noreply.github.com>
2019-05-04 13:12:37 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Event
+$('.page-scroll').on('click', function(e){
+	// Ambil isi href
+	var tujuan = $(this).attr('href');
+	// Ambil Elemen href
+	var elemenTujuan = $(tujuan);
+
+	// Pindahkan Nyekrol
+	$('body').animate({
+		scrollTop: elemenTujuan.offset().top -50
+	}, 1250, 'easeInOuExpo');
+
+	e.preventDefault();
+
+});
