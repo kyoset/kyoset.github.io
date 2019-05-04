ed7149c8ce25d583326271e84768b6260345459d
kyoset <46853509+kyoset@users.noreply.github.com>
2019-05-04 15:49:17 +0000

    JavaScript

--- js/script.js
+++ js/script.js
@@ -11,3 +11,37 @@ $('.page-scroll').on('click', function(e) {
 
  e.preventDefault();
 });﻿
+
+// Parallax
+     // About Text
+     $(window).on('load', function() {
+     	$('.pleft').addClass('pshow');
+     	$('.pright').addClass('pshow');
+     });
+
+$(window).scroll(function(){
+	var iScroll = $(this).scrollTop();
+
+	// Jumbotron
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ iScroll/4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ iScroll/2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ iScroll/1.2 +'%)'
+	});
+
+	// Portfolio
+	if( iScroll > $('.portfolio').offset().top -250 ) {
+		$('.portfolio .thumbnail').each(function(i) {
+			setTimeout(function() {
+				$('.portfolio .thumbnail').eq(i).addClass('foto');
+			}, 300 * (i+1));
+		});
+}
+
+});
